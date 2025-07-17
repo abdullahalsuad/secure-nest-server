6c5b1c0f08c83553ab4bc189f3057209de6a49da
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-07-17 22:07:50 +0600

    origin added

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://secure-nest-client.vercel.app/"],
+    origin: ["http://localhost:5173", "https://secure-nest-client.vercel.app"],
     credentials: true,
   })
 );
